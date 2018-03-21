f38a04cff533ecaf447c268595c2d6fa616bcb7d
Leonid Boykov <lordkratos.47@gmail.com>
2018-03-21 22:27:35 +0300

    added: print version on launch

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/kelseyhightower/envconfig"
@@ -10,17 +11,24 @@ import (
 	"github.com/smarthut/agent/router"
 )
 
+var (
+	version = "master"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
 	var conf device.Configuration
 	if err := envconfig.Process("agent", &conf); err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 
 	if err := device.Create(conf); err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 
 	l := fmt.Sprintf("%s:%d", conf.Host, conf.Port)
 
+	log.Printf("Starting SmartHut Agent %s on %s\n", version, l)
 	http.ListenAndServe(l, router.New())
 }
